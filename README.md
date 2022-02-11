fa9cde5f25176c160f61ca2a89ba17127f33dadd
Franco Guaragna <55494072+fguaragna97@users.noreply.github.com>
2022-02-10 21:54:01 -0800

    Set Up config

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # instagram-api
- 
+
+## Install
+
+1. `npm install`
+2. Copy `.env.example` and rename to `.env`
+3. `npm start`
--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const http = require("http");
 const app = require("./server");
-
-const hostname = "127.0.0.1";
-const port = 3000;
+const { port } = require("./server/config");
 
 const server = http.createServer(app);
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running at ${port}`);
 });
